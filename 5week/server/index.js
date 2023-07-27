16f0b3fc3d9264ad1970556be761134ec4bfa3bf
Ubuntu <ubuntu@ip-172-31-88-198.ec2.internal>
2023-07-27 07:21:32 +0000

    set

---
+++ 5week/server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql');
+const dbconfig = require('./dbinfo.js');
+const connection = mysql.createConnection(dbconfig);
+
+const app = express();
+const port = 3000;
+const path = require('path');
+const cors = require('cors');
+
+app.get('/api',(req, res) => {
+	connection.query('SELECT * FROM test', (error, rows) => {
+		if (error) throw error;
+		res.send(rows);
+	});
+});
+
+app.get('/',(req, res) => {
+	res.sendFile(path.join(__dirname, '../', 'html', 'Main.html'));
+});
+
+app.listen(port, () => {
+	console.log('Example app listening on port ' + port);
+});
+
+app.use(
+	cors({
+		origin : 'http://54.174.107.46:3000',
+		credentials : true
+	})
+);
+
