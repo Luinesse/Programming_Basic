f509a2fe219e009eb8c2bc0fecec59cbd009a0ea
root <root@DESKTOP-FHNLA9O>
2023-07-30 15:07:46 +0900

    test

--- 5week/server/public/js/Main.js
+++ 5week/server/public/js/Main.js
@@ -4,6 +4,7 @@ const mainPg = document.getElementById('Main-page');
 const titleH = document.getElementById("goMain");
 const writePg = document.getElementById('write-btn');
 const state = document.querySelector(".sign-text");
+const userhi = document.querySelector(".hi");
 
 if(document.cookie.indexOf('user=') === -1) {
     state.textContent = 'Login';
@@ -12,7 +13,6 @@ if(document.cookie.indexOf('user=') === -1) {
     });
 }
 else {
-    const userhi = document.querySelector(".hi");
     userhi.textContent = document.cookie.split('user=')[1].split(';')[0] + '님 환영합니다.';
     state.textContent = 'Logout';
     state.addEventListener('click', e => {
