ff9f7eba4db3a4025a6484b309ba85cf8a795a13
root <root@DESKTOP-FHNLA9O>
2023-07-30 13:34:37 +0900

    test

--- 5week/server/public/js/Main.js
+++ 5week/server/public/js/Main.js
@@ -4,14 +4,12 @@ const loginPg = document.getElementById('Login-page');
 const mainPg = document.getElementById('Main-page');
 const titleH = document.getElementById("goMain");
 const writePg = document.getElementById('write-btn');
-const cookies = "<%= cookie %>";
 
-if(cookies === "false") {
-    document.querySelector(".sign-text").innerHTML = "로그인";
-    document.querySelector(".sign-text").href = "/login";
-} else {
-    document.querySelector(".sign-text").innerHTML = "로그아웃";
-    document.querySelector(".sign-text").href = "/logout";
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp (
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
 function moveToWrite() {
@@ -20,6 +18,14 @@ function moveToWrite() {
 
 function moveToMain() {
     location.href = "/";
+}
+
+if(getCookie(user) != undefined) {
+    document.querySelector(".sign-text").innerHTML = "로그인";
+    document.querySelector(".sign-text").href = "/login";
+} else {
+    document.querySelector(".sign-text").innerHTML = "로그아웃";
+    document.querySelector(".sign-text").href = "/logout";
 }
 
 mainPg.addEventListener("click",moveToMain);
