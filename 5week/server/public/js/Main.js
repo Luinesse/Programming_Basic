afa46dae9ae6046c30ac0b76248136e2246887d0
root <root@DESKTOP-FHNLA9O>
2023-07-30 13:28:44 +0900

    test

--- 5week/server/public/js/Main.js
+++ 5week/server/public/js/Main.js
@@ -4,6 +4,15 @@ const loginPg = document.getElementById('Login-page');
 const mainPg = document.getElementById('Main-page');
 const titleH = document.getElementById("goMain");
 const writePg = document.getElementById('write-btn');
+const cookies = "<%= cookie %>";
+
+if(cookies === "false") {
+    document.querySelector(".sign-text").innerHTML = "로그인";
+    document.querySelector(".sign-text").href = "/login";
+} else {
+    document.querySelector(".sign-text").innerHTML = "로그아웃";
+    document.querySelector(".sign-text").href = "/logout";
+}
 
 function moveToWrite() {
     location.href = "/write";
@@ -13,11 +22,6 @@ function moveToMain() {
     location.href = "/";
 }
 
-function moveToLogin() {
-    location.href = "/login";
-}
-
-loginPg.addEventListener("click",moveToLogin);
 mainPg.addEventListener("click",moveToMain);
 writePg.addEventListener("click",moveToWrite);
 titleH.addEventListener("click",moveToMain);
