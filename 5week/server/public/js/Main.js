e0a3b9ef7797774efa3035660213c27ce6190f40
root <root@DESKTOP-FHNLA9O>
2023-07-30 13:50:28 +0900

    test

--- 5week/server/public/js/Main.js
+++ 5week/server/public/js/Main.js
@@ -4,15 +4,6 @@ const loginPg = document.getElementById('Login-page');
 const mainPg = document.getElementById('Main-page');
 const titleH = document.getElementById("goMain");
 const writePg = document.getElementById('write-btn');
-const cookies = "<%= user %>";
-
-if(cookies === "false") {
-    document.querySelector(".sign-text").innerHTML = "Login";
-    document.querySelector(".sign-text").href = "/login";
-} else {
-    document.querySelector(".sign-text").innerHTML = "Logout";
-    document.querySelector(".sign-text").href = "/logout";
-}
 
 function moveToWrite() {
     location.href = "/write";
