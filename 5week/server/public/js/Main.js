75613108f051697f8b138fecff3395d4706bd860
root <root@DESKTOP-FHNLA9O>
2023-07-30 14:03:57 +0900

    test

--- 5week/server/public/js/Main.js
+++ 5week/server/public/js/Main.js
@@ -1,9 +1,35 @@
 "use strict";
 
-const loginPg = document.getElementById('Login-page');
+//const loginPg = document.getElementById('Login-page');
 const mainPg = document.getElementById('Main-page');
 const titleH = document.getElementById("goMain");
 const writePg = document.getElementById('write-btn');
+
+function getCookie(name) {
+    var nameOfCookie = name + "=";
+    var x = 0;
+    while(x <= document.cookie.length) {
+        var y = (x + nameOfCookie.length);
+        if(document.cookie.substring(x,y) == nameOfCookie) {
+            if((endOfCookie = document.cookie.indexOf(";",y)) == -1)
+                endOfCookie = document.cookie.length;
+            return unescape(document.cookie.substring(y, endOfCookie));
+        }
+        x = document.cookie.indexOf("",x) + 1;
+        if(x == 0)
+            break;
+    }
+    return "";
+}
+
+var cookie = getCookie("user");
+if(cookie != "") {
+    document.querySelector(".sign-text").innerHTML = "Logout";
+    document.querySelector(".sign-text").href = "./logout";
+} else {
+    document.querySelector(".sign-text").innerHTML = "Login";
+    document.querySelector(".sign-text").href = "./login";
+}
 
 function moveToWrite() {
     location.href = "/write";
@@ -13,11 +39,11 @@ function moveToMain() {
     location.href = "/";
 }
 
-function moveToLogin() {
-    location.href = "/login";
-}
+//function moveToLogin() {
+//    location.href = "/login";
+//}
 
-loginPg.addEventListener("click",moveToLogin);
+//loginPg.addEventListener("click",moveToLogin);
 mainPg.addEventListener("click",moveToMain);
 writePg.addEventListener("click",moveToWrite);
 titleH.addEventListener("click",moveToMain);
