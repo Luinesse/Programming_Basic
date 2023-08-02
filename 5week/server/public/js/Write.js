c07bab09e28bc966062673c991c99c955b5fbfba
root <root@DESKTOP-U0H4RQ4>
2023-08-02 17:23:41 +0900

    test

--- 5week/server/public/js/Write.js
+++ 5week/server/public/js/Write.js
@@ -3,19 +3,19 @@
 const mainPg = document.getElementById("Main-page");
 const titleH = document.getElementById("goMain");
 const uploadBtn = document.getElementById("upload-btn");
-const state = document.querySelector(".sign-text");
-const userhi = document.querySelector(".hi");
+const wstate = document.querySelector(".write-text");
+const wuserhi = document.querySelector(".write-hi");
 
 if(document.cookie.indexOf('user=') === -1) {
-    state.textContent = 'Login';
-    state.addEventListener('click', e => {
+    wstate.textContent = 'Login';
+    wstate.addEventListener('click', e => {
         location.href = '/login';
     });
 }
 else {
-    userhi.textContent = document.cookie.split('user=')[1].split(';')[0] + '님 환영합니다.';
-    state.textContent = 'Logout';
-    state.addEventListener('click', e => {
+    wuserhi.textContent = document.cookie.split('user=')[1].split(';')[0] + '님 환영합니다.';
+    wstate.textContent = 'Logout';
+    wstate.addEventListener('click', e => {
         location.replace("/logout");
     });
 }
