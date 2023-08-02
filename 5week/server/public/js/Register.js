6eabd0050b9e6c47ac438acaa9d3afdf6c6c1987
root <root@DESKTOP-U0H4RQ4>
2023-08-02 18:20:28 +0900

    test

--- 5week/server/public/js/Register.js
+++ 5week/server/public/js/Register.js
@@ -1,23 +1,27 @@
 "use strict";
 
+const { format } = require("mysql");
+
 const regisId = document.getElementById('register-id');
 const regisPw = document.getElementById('register-pw');
-const regisBut = document.getElementById('register-btn');
+const regisForm = document.getElementById('register-form');
 
-function checkPW()
-{
+//regisBut.addEventListener("click",checkPW);
+regisForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
     var regexPw = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/;
 
     if(regisId.value.length < 5)
     {
         alert("아이디는 5자 이상이어야 합니다.");
-        return false;
+        return;
     }
     else if(!regexPw.test(regisPw.value))
     {
         alert("8~20자 공백을 제외한 영문 대소문자, 숫자, 특수문자를 사용하세요.");
-        return false;
+        return;
     }
-}
 
-regisBut.addEventListener("click",checkPW);
+    regisForm.submit();
+});
