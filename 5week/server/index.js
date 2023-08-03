7942c974178068036e4cb7cbdb09464a69ebdfd4
root <root@DESKTOP-FHNLA9O>
2023-08-04 05:13:27 +0900

    test

--- 5week/server/index.js
+++ 5week/server/index.js
@@ -127,7 +127,7 @@ app.get('/write',(req, res) => {
 		res.send('<script type="text/javascript">alert("로그인 후 이용해 주세요."); location.href="/";</script>');
 });
 
-app.get('/board/api/:bid', (req, res) => {
+app.get('/board', (req, res) => {
 	res.sendFile(path.join(__dirname, './public', 'html', 'Board.html'));
 });
 
