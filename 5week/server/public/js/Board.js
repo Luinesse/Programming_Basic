6698bddd29a35e3f0253f78198907100c8a7ec2a
root <root@DESKTOP-FHNLA9O>
2023-08-04 13:47:27 +0900

    test

--- 5week/server/public/js/Board.js
+++ 5week/server/public/js/Board.js
@@ -49,9 +49,11 @@ else {
 function deleteAct() {
     let input = confirm('정말로 게시글을 삭제하시겠습니까 ?');
     if(input) {
-        let value = document.cookie.split('user=')[1].split(';')[0];
-        if(value === "") {
-            alert("로그인 후 이용해 주세요.");
+        let value;
+        if(!document.cookie.split('user=')[1].split(';')[0]) {
+            alert("로그인 후 이용해주세요.");
+        } else {
+            value = document.cookie.split('user=')[1].split(';')[0];
         }
         let delPw = prompt('비밀번호를 입력해주세요.');
 
