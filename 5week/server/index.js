34d294893d4a388dedb33dfedf2cfc3ca2f4d9b9
root <root@DESKTOP-FHNLA9O>
2023-08-15 23:46:31 +0900

    test

--- 5week/server/index.js
+++ 5week/server/index.js
@@ -251,10 +251,6 @@ app.get('/api/posts',(req, res) => {
 	});
 });
 
-app.post('*', csrfProtection, (req, res, next) => {				//next는 *로 해당 주소가 들어왔을 때, csrfProtection 함수를 통해 csrf토큰을 검사하고, next() 호출로 원래 들어왔던 주소 ex) app.post('/', (req, res) => {}) 로 핸들을 넘김.
-	next();
-})
-
 app.get('/comment/api/:bid', (req, res) => {
 	connection.query('SELECT cid, text, replyDate, username FROM commentInfo WHERE boardInfo_bid = ?', [req.params.bid], (error, results, fields) => {
 		if(error)	throw error;
