77777507f20f2a6f24ca263c479852ca72c9aaa7
root <root@DESKTOP-FHNLA9O>
2023-08-15 21:26:58 +0900

    test

--- 5week/server/index.js
+++ 5week/server/index.js
@@ -53,15 +53,10 @@ app.post('/login', (req, res) => {
 					if(req.session.user)
 						res.redirect("/");
 					else {
-						req.session = {
-							user: {
-								id : req.body.id,
-							},
-							cookie: {
-								maxAge: 1000 * 60 * 60 * 24 * 7
-							}
+						req.session.user = {
+							id : req.body.id,
 						};
-						
+
 						res.setHeader('Set-Cookie', ['user=' + req.body.id]);
 						res.redirect("/");
 					}
