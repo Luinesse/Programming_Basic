f70eb0b6053b5b1a2e39774e175261e551a77d3b
root <root@DESKTOP-FHNLA9O>
2023-08-15 21:44:48 +0900

    test

--- 5week/server/index.js
+++ 5week/server/index.js
@@ -107,7 +107,7 @@ app.post('/write', (req, res) => {
 	if(wrote_title && wrote_article) {
 		connection.query('SELECT uid FROM userInfo WHERE id = ?', [req.session.user.id], (error, results, fields) => {
 			if(error)	throw error;
-			connection.query('INSERT INTO boardInfo (title, article, username, boardDate, userInfo_uid) VALUES(?,?,?,CURRENT_TIMESTAMP,?)', [wTitle, wArticle, req.session.user.id, results[0].uid], (error, data) => {
+			connection.query('INSERT INTO boardInfo (title, article, username, boardDate, userInfo_uid) VALUES(?,?,?,CURRENT_TIMESTAMP,?)', [wrote_title, wArticle, req.session.user.id, results[0].uid], (error, data) => {
 				if(error)	throw error2;
 				res.send('<script type="text/javascript">alert("작성이 완료됐습니다."); location.replace("/");</script>');
 			});
